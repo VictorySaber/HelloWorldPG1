31cb91cb2cedbdc3fc67cc7a50df1301ae59f52b
VictorySaber <nightstalker98@hotmail.com>
2017-02-04 11:42:49 +0000

    mistake in upload

--- www/placeholder.js
+++
@@ -1,8 +0,0 @@
-
-jQuery(document).ready(function(){
-	
-	$('.activation-form input[type="text"], .activation-form input[type="password"]).each(function() {
-		$(this).val( $(this).attr('placeholder') );
-    });
-	
-});
